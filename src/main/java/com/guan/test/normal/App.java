cba5542e6e5e7bb817bebae7781a557a09cc6597
guanhaobi <guanhaobi@163.com>
2017-08-27 21:07:40 +0800

    update test case

---
+++ src/main/java/com/guan/test/normal/App.java
@@ -0,0 +1,13 @@
+package com.guan.test.normal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/guan/test/normal/bingfa/HelloWorld.java
@@ -0,0 +1,33 @@
+
+package com.guan.test.normal.bingfa;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.junit.Test;
+
+public class HelloWorld
+{
+
+    @Test
+    /**
+     * 锁分段技术实现
+     */
+    public void test001()
+    {
+        int capacity = 16;
+        float loadFactor = 0.75f;
+        int concurrencyLevel = 16;// 并发发的线程数保持一致比较好
+        ConcurrentHashMap<String, String> cHashMap = new ConcurrentHashMap<>(
+                capacity, loadFactor, concurrencyLevel );
+
+        String key = "helloKey";
+        String value = "asdfa";
+
+        cHashMap.put( key, value );
+
+        cHashMap.replace( key, "asdfa", "aaa" );
+
+        System.out.println( cHashMap.get( key ) );
+    }
+
+}
