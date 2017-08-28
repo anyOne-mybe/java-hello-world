a8750e119e1766019be204c0619f29c0d093b333
guanhaobi1 <guanhaobi1@999.com.cn>
2017-08-28 16:38:59 +0800

    add sorcket channel codes

---
+++ src/main/java/com/guan/test/nio/client/Client.java
@@ -0,0 +1,27 @@
+
+package com.guan.test.nio.client;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+public class Client
+{
+    public static void main( String[] args ) throws IOException
+    {
+        SocketChannel socketChannel = SocketChannel.open();
+        socketChannel.connect( new InetSocketAddress( "127.0.0.1", 5000 ) );
+        ByteBuffer writeBuffer = ByteBuffer.allocate( 32 );
+        ByteBuffer readBuffer = ByteBuffer.allocate( 32 );
+        writeBuffer.put( "hello".getBytes() );
+        writeBuffer.flip(); // make buffer ready for reading
+
+        writeBuffer.rewind(); // sets the position back to 0
+        socketChannel.write( writeBuffer ); // hello
+        readBuffer.clear(); // make buffer ready for writing
+        socketChannel.read( readBuffer ); // recieved
+
+        socketChannel.close();
+    }
+}
